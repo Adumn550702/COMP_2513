2fe53a870ca16949abee3a552c67880a01cb9632
Jamie Symonds <jamie.symonds@gmail.com>
2023-02-14 13:14:38 -0400

    Added PHP Examples

--- AJAX_Examples/example1.js
+++ AJAX_Examples/example1.js
@@ -1,36 +1,47 @@
-//example: https://data.novascotia.ca/resource/2shh-dv8p.json?facility=TIM%20HORTONS&facility_town=YARMOUTH
-
 
 function getDataFromAPI() {
-    //example: https://data.novascotia.ca/resource/2shh-dv8p.json?facility_town=YARMOUTH
 
     var facility_town = $("#facility_town").val();
+    var facility = $("#facility").val();
 
-    var url = "https://data.novascotia.ca/resource/2shh-dv8p.json?"; 
-        url +=  "facility_town=" + facility_town.toUpperCase();
+    if (facility_town == "" || facility == "") {
+        alert("You need to enter a town name.");
+    } else {
+        $("#loading").show();
 
-    $.get(url, function(data, status){
-        console.log(data, status);
-        //alert the user if nothing was found, otherwise show the results.
-        if (data.length == 0) {
-            alert("no results returned.");
-            //empty the results container (in case it contained data from a previous search) - always clean up after yourself
-            $("#results_container").html("<br/>");
-        } else {
-            //iterate over the array of results and brutally construct some HTML and dump it into the container (div)
-            var htmlString = "<p>" + data.length + " results found.</p>";
-            htmlString += "<ul>";
-            for(var i = 0; i < data.length; i++) {
-                htmlString += "<li>" + data[i].facility + " (" + data[i].facility_address + ")</li>";
+        var url = "https://data.novascotia.ca/resource/2shh-dv8p.json?";
+            url += "facility_town=" + facility_town.toUpperCase() + "&facility=" + facility.toUpperCase();
+    
+        $.get(url, function(data, status){
+            console.log(data, status);
+    
+            $("#results_container").html("");
+    
+            var resultHTML = "<h1>" + facility + " in " + facility_town + "</h1><p>" + data.length + " results returned.</p><ul>";
+            if(data.length == 0) {
+                alert("no matches found, try again...");
+            } else {
+                for (var i = 0; i < data.length; i++) {
+                    var restuarant_name = data[i].facility;
+                    var street_address = data[i].facility_address;
+                    resultHTML += "<li>" + restuarant_name + " " + street_address + "</li>"
+                }
             }
-            htmlString += "</ul">
-
-            $("#results_container").html(htmlString);
-        }
-    });    
+            resultHTML += "</ul>";
+    
+            $("#facility_town").val("");
+            $("#facility").val("");
+            $("#facility_town").focus();
+            $("#loading").hide();
+            $("#results_container").html(resultHTML);
+        });
+    }
 }
 
 window.addEventListener("load", (event) => {
+
+    $("#loading").hide();
+
     const el = document.getElementById("getdata_btn");
     el.addEventListener("click", getDataFromAPI, false);
 });
